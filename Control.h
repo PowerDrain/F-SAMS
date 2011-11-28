926842b1e0bb5b521187ed120bac1594a24296d8
David Gamez <powerdrain@gmail.com>
2011-11-28 16:42:12 -0600

    Created .h file for our current Control.cpp file.

---
+++ Control.h
@@ -0,0 +1,11 @@
+#ifndef CONTROL
+#define CONTROL
+
+public String fire_event(int zoneID);
+public String fire_test_event(int zoneID);
+public security_event(int zoneID);
+public security_test_event(int zoneID);
+public turn_off(int zoneID, String password);
+public turn_off(int zoneID, String password, String alarm_type);
+
+#endif
